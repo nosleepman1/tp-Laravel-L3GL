169b70a1f41f51e4640df765e32b57efa48a4b8f
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-02-17 01:19:51 +0000

    ajout du formulaire de du tableau de donnees recuperés

--- app/routes/web.php
+++ app/routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+
+    $tasks = ['task1', 'task2'];
+    return view('welcome', [
+        'tasks'=> $tasks,
+    ]);
 });
